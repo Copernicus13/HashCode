c198c528b10447c08f125ad9a459140628f477c1
Pascal Crouzet <DudePascalou@users.noreply.github.com>
2016-02-06 21:22:19 +0000

    Ajout des projets console et de test.

---
+++ Google.HashCode.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Google.HashCode.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Just a test...
+            Assert.IsTrue(true);
+        }
+    }
+}
