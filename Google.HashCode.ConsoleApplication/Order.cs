0f536800c0e003b427c3abe70d20665e39d01d56
Michel Pellegrin <Copernicus13@users.noreply.github.com>
2016-02-11 18:52:32 +0000

    Input parsing

---
+++ Google.HashCode.ConsoleApplication/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Google.HashCode.ConsoleApplication
+{
+    public class Order
+    {
+        public Point Destination { get; set; }
+
+        public IList<int> NbItemsOfType { get; set; }
+
+        public Order(int nbItemsTotal)
+        {
+            NbItemsOfType = Enumerable.Repeat(0, nbItemsTotal).ToList();
+        }
+    }
+}
---
+++ Google.HashCode.ConsoleApplication/Warehouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Google.HashCode.ConsoleApplication
+{
+    public class Warehouse
+    {
+        public Point Position { get; set; }
+
+        public IList<int> NbItemsOfType { get; set; }
+
+        public Warehouse(int nbProductTotal)
+        {
+            NbItemsOfType = Enumerable.Repeat(0, nbProductTotal).ToList();
+        }
+    }
+}
