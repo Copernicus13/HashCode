f00a6cf43752c7e9de0002143b2c99c263645a4d
Keuvain <k94ll13nn3@users.noreply.github.com>
2016-02-11 20:28:51 +0000

    Add start of Solver class and associated tests

---
+++ Google.HashCode.Tests/SolverTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Google.HashCode.ConsoleApplication;
+using NUnit.Framework;
+
+namespace Google.HashCode.Tests
+{
+    [TestFixture]
+    internal class SolverTests
+    {
+        [Test]
+        public void CanComputeProductGroups()
+        {
+            var solver = new Solver(new Program(true) { maxPayload = 400 });
+            var productList = new[] { 1, 2, 3 };
+            var weightList = new[] { 100, 50, 200 };
+
+            IList<IEnumerable<int>> expectedList  = new[] { new[] { 2, 2 }, new[] { 2, 0, 1, 1 } };
+
+            var actual = solver.ComputeProductGroups(productList, weightList);
+            Assert.That(actual, Is.EqualTo(expectedList));
+        }
+    }
+}
