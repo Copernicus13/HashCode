8b8080c2ab19fe2d7a79cf8d33e5efa3e51aff17
pcrouzet <pcrouzet@sodifrance.fr>
2017-02-23 21:26:54 +0100

    Added a batch file to loop over input files and output to files.

--- 2017/HashCode/Execute.bat
+++ 2017/HashCode/Execute.bat
@@ -1,1 +1,4 @@
-﻿
+HashCode.exe < Input\me_at_the_zoo.in 1> Output\me_at_the_zoo.out 2> Output\me_at_the_zoo.err
+HashCode.exe < Input\videos_worth_spreading.in 1> Output\videos_worth_spreading.out 2> Output\videos_worth_spreading.err
+HashCode.exe < Input\trending_today.in 1> Output\trending_today.out 2> Output\trending_today.err
+HashCode.exe < Input\kittens.in 1> Output\kittens.out 2> Output\kittens.err
