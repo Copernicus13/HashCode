bb43347cee8f75d4f6e269b464eededd49f93f9b
pcrouzet <pcrouzet@sodifrance.fr>
2017-02-23 19:47:25 +0100

    Added RequestsCount property on Request.

--- 2017/HashCode/Request.cs
+++ 2017/HashCode/Request.cs
@@ -9,6 +9,7 @@ namespace HashCode
     public class Request
     {
         public int RequestId { get; set; }
+        public int RequestsCount { get; set; }
         public Video Video { get; set; }
         public Endpoint Endpoint { get; set; }
     }
