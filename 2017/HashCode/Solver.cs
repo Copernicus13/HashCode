7891df803deb8452248c3411957d2befe9c3c044
mbonn <mbonn@sodifrance.fr>
2017-02-23 19:58:00 +0100

    Round Kévin + Mathieu

--- 2017/HashCode/Solver.cs
+++ 2017/HashCode/Solver.cs
@@ -20,5 +20,53 @@ namespace HashCode
             this.Endpoints = new List<Endpoint>();
             this.Requests = new List<Request>();
         }
+
+        public void Solve()
+        {
+            var listeResultat = new List<Result>();
+            foreach (var video in Videos)
+            {
+                var resultatPourVideo = new Result { VideoId = video.VideoId };
+                var requests = Requests.Where(request => request.Video.VideoId == video.VideoId);
+                var endpoints = requests.Select(request => request.Endpoint);
+
+                if (video.Size > CacheSize)
+                {
+                    resultatPourVideo.MinSumLatency = requests.Sum(requete => requete.Endpoint.Latency * requete.RequestsCount);
+                    resultatPourVideo.Endpoints = requests.Select(requete => Tuple.Create(requete.Endpoint.EndpointId, -1)).ToList();
+                }
+                else
+                {
+                    resultatPourVideo.MinSumLatency = 0;
+                    resultatPourVideo.Endpoints = new List<Tuple<int, int>>();
+                    foreach (var requete in requests)
+                    {
+
+                        var meilleurCache = requete.Endpoint.Caches.OrderBy(cache => cache.Latency).FirstOrDefault();
+                        if (meilleurCache != null && meilleurCache.Latency <= requete.Endpoint.Latency)
+                        {
+                            resultatPourVideo.MinSumLatency += meilleurCache.Latency * requete.RequestsCount;
+                            resultatPourVideo.Endpoints.Add(Tuple.Create(requete.Endpoint.EndpointId, meilleurCache.CacheId));
+                        }
+                        else
+                        {
+                            resultatPourVideo.MinSumLatency += requete.Endpoint.Latency * requete.RequestsCount;
+                            resultatPourVideo.Endpoints.Add(Tuple.Create(requete.Endpoint.EndpointId, -1));
+                        }
+                    }
+                }
+
+                listeResultat.Add(resultatPourVideo);
+            }
+
+
+        }
+
+        private class Result
+        {
+            public int VideoId { get; set; }
+            public long MinSumLatency { get; set; }
+            public ICollection<Tuple<int, int>> Endpoints { get; set; }
+        }
     }
 }
