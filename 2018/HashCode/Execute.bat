da3d5948c88a8bd04e1c1ed35b4885a2a10e934a
Pascal Crouzet <dude.pascalou@gmail.com>
2018-02-28 21:56:30 +0100

    Added 2018 template.

---
+++ 2018/HashCode/Execute.bat
@@ -0,0 +1,4 @@
+HashCode.exe < Input\file1.in 1> Output\file1.out 2> Output\file1.err
+HashCode.exe < Input\file2.in 1> Output\file2.out 2> Output\file2.err
+HashCode.exe < Input\file3.in 1> Output\file3.out 2> Output\file3.err
+HashCode.exe < Input\file4.in 1> Output\file4.out 2> Output\file4.err
